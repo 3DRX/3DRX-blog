ffcb1430b5729927320ada2b9ee909521aa0a69e
3DRX <kjy20030228@icloud.com>
2023-06-14 17:04:23 +0800

    Update README, create dark mode.

---
+++ public/lib/theme-util.js
@@ -0,0 +1,30 @@
+const storageKey = 'theme-preference'
+
+const getColorPreference = () => {
+    let preference = localStorage.getItem(storageKey);
+
+    if (!preference) {
+        preference = window.matchMedia('(prefers-color-scheme: dark)').matches
+            ? 'dark'
+            : 'light';
+    }
+
+    return preference;
+}
+
+const setPreference = (themeName) => {
+    localStorage.setItem(storageKey, themeName)
+
+    document.firstElementChild
+        .setAttribute('data-theme', themeName);
+}
+
+const togglePreference = () => {
+    setPreference(getColorPreference() === 'dark' ? 'light' : 'dark');
+}
+
+(() => {
+    const theme = getColorPreference();
+
+    setPreference(theme);
+})();
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "3DRX-blog";
+export const SITE_DESCRIPTION = "My blog about learning computer science and programming.";
