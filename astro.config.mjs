89c0fec6d026cb7385e2b57e7691d8f975d447cb
3DRX <kjy20030228@icloud.com>
2023-06-14 18:09:38 +0800

    Edit posts, enable katex rendering in markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap()],
+    markdown: {
+        remarkPlugins: [
+            'remark-math',
+        ],
+        rehypePlugins: [
+            ['rehype-katex', {
+                // Katex plugin options
+                strict: false,
+            }]
+        ]
+    },
+    experimental: {
+        assets: true,
+    },
 });
