3a36ad92272c88e92b41a5256387f081717074ab
3DRX <kjy20030228@icloud.com>
2023-06-25 20:03:27 +0800

    Add RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,9 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function get(context) {
-	const posts = await getCollection('blog');
+    const posts = (await getCollection('blog')).sort(
+        (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+    );
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
