438bbb6e90e75c87aefce0a15fc24074c773bd05
3DRX <kjy20030228@icloud.com>
2023-09-08 10:19:00 +0800

    start editing 极速前进.md

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,4 +18,18 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
+const photography = defineCollection({
+    // Type-check frontmatter using a schema
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        // Transform string to Date object
+        pubDate: z
+            .string()
+            .or(z.date())
+            .transform((val) => new Date(val)),
+        heroImage: z.string().optional(),
+    }),
+});
+
+export const collections = { blog, photography };
