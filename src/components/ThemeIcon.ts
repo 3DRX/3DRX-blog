b1cf2f34f064bfd5da3322d521ad27e575327c6f
3DRX <kjy20030228@icloud.com>
2023-12-06 11:28:08 +0800

    Redo ThemeIcon component, add view transition.

---
+++ src/components/ThemeIcon.ts
@@ -0,0 +1,35 @@
+import { THEME_STORAGE_KEY } from "../consts";
+
+function getColorPreference() {
+  let preference = localStorage.getItem(THEME_STORAGE_KEY);
+  if (!preference) {
+    preference = window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  }
+  return preference;
+}
+
+function setPreference(themeName: string) {
+  localStorage.setItem(THEME_STORAGE_KEY, themeName);
+  if (document.firstElementChild) {
+    document.firstElementChild.setAttribute("data-theme", themeName);
+  }
+}
+
+function togglePreference() {
+  setPreference(getColorPreference() === "dark" ? "light" : "dark");
+}
+
+const buttons = document.querySelectorAll("input.theme-toggle");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    togglePreference();
+  });
+});
+
+setPreference(getColorPreference());
+
+document.addEventListener("astro:after-swap", () => {
+  setPreference(getColorPreference());
+});
--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = "3DRX's blog";
 export const SITE_DESCRIPTION =
   "My blog about learning computer science and programming.";
+export const THEME_STORAGE_KEY = "theme-preference";
