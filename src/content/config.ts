0d24f235afd6ebd302870a166aed8d3066dd02af
3DRX <kjy20030228@icloud.com>
2023-12-08 11:01:50 +0800

    Remove unused rss.xml.js, fix category not being displayed in blog index

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,9 @@
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    // Transform string to Date object
     pubDate: z
       .string()
       .or(z.date())
@@ -15,16 +13,13 @@ const blog = defineCollection({
       .optional()
       .transform((str) => (str ? new Date(str) : undefined)),
     heroImage: z.string().optional(),
-    catagory: z.string().optional(),
   }),
 });
 
 const photography = defineCollection({
-  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    // Transform string to Date object
     pubDate: z
       .string()
       .or(z.date())
