62c7942e4f9b75283a1a4f64a61abdd14553479b
3DRX <kjy20030228@icloud.com>
2024-01-11 20:26:06 +0800

    Refactor SkylineRender.js to Skyline.js

---
+++ src/components/Skyline.js
@@ -0,0 +1,64 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+function render() {
+  const scene = new THREE.Scene();
+  let model;
+  new GLTFLoader().load(
+    `/3DRX-2022.gltf`,
+    (gltf) => {
+      model = gltf.scene;
+      scene.add(model);
+    },
+    (_) => {},
+    (error) => {
+      console.log("error", error);
+    },
+  );
+  const canvas = document.querySelector(".webgl");
+  if (!canvas) {
+    return;
+  }
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+  directionalLight.position.set(40, 8, 0);
+  scene.add(directionalLight);
+  const light3 = new THREE.PointLight(0xffffff, 8000, 200);
+  light3.position.set(0, 30, 0);
+  scene.add(light3);
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    canvas.offsetWidth / canvas.offsetHeight,
+    0.1,
+    300,
+  );
+  camera.position.z = 112;
+  scene.add(camera);
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+    alpha: true,
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+  const mouse = new THREE.Vector2();
+  const isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent,
+    );
+  if (!isMobile) {
+    window.addEventListener("mousemove", (event) => {
+      mouse.x = event.clientX / window.innerWidth - 0.5;
+      mouse.y = event.clientY / window.innerHeight - 1;
+    });
+  }
+  function loop() {
+    window.requestAnimationFrame(loop);
+    const rotationSpeed = 0.1;
+    model.rotation.x = mouse.y * rotationSpeed - 1.1;
+    model.rotation.z = mouse.x * rotationSpeed;
+    renderer.render(scene, camera);
+  }
+  loop();
+}
+render();
+document.addEventListener("astro:after-swap", render);
