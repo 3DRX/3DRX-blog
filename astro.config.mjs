384856cf159724e160cb92411e963036c04654cd
3DRX <kjy20030228@icloud.com>
2024-03-09 00:39:51 +0800

    Change the way markdown related plugins are loaded
    so that remark-math and rehype-katex can handle mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
   site: "https://www.3drx.top/",
   integrations: [sitemap()],
   markdown: {
-    remarkPlugins: ["remark-math"],
-    rehypePlugins: [["rehype-katex", { strict: false }]],
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [[rehypeKatex, { strict: false }]],
   },
   prefetch: {
     defaultStrategy: "hover",
