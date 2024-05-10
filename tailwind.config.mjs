ec2c14ca55dd8bf9068381c71b66ab1936368e67
3DRX <kjy20030228@icloud.com>
2024-05-11 02:47:45 +0800

    start refactoring styles to tailwind.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{js,jsx,md,mdx,ts,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,js,jsx,md,mdx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
