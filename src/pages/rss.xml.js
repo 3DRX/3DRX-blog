8d4c6c0822788f1665e310ba9ad191f5a382ec70
3DRX <3drxkjy@gmail.com>
2024-06-27 14:10:52 +0800

    .

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,23 +1,21 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
-import MarkdownIt from "markdown-it";
+import sanitizeHtml from "sanitize-html";
 import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
-const parser = new MarkdownIt();
 
 export async function GET(context) {
-  const blog = await getCollection("blog");
+  const postImportResult = import.meta.glob("../content/blog/**/*.md", {
+    eager: true,
+  });
+  const posts = Object.values(postImportResult);
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: blog
-      .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+    items: posts
       .map((post) => ({
-        title: post.data.title,
-        description: post.data.description,
-        pubDate: post.data.pubDate,
-        link: `/blog/${post.slug}`,
-        content: parser.render(post.body),
+        link: post.url,
+        content: sanitizeHtml(post.compiledContent()),
+        ...post.frontmatter,
       })),
   });
 }
