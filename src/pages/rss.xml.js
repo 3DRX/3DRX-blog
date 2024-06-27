f80143883113dc2366255f54978ce11ad9058001
3DRX <3drxkjy@gmail.com>
2024-06-27 14:57:09 +0800

    .

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,21 +1,21 @@
 import rss from "@astrojs/rss";
 import sanitizeHtml from "sanitize-html";
+import { marked } from "marked";
+import { getCollection } from "astro:content";
 import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function GET(context) {
-  const postImportResult = import.meta.glob("../content/blog/**/*.md", {
-    eager: true,
-  });
-  const posts = Object.values(postImportResult);
+  const posts = (await getCollection("blog")).sort(
+    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
+  );
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts
-      .map((post) => ({
-        link: post.url,
-        content: sanitizeHtml(post.compiledContent()),
-        ...post.frontmatter,
-      })),
+    items: posts.map((post) => ({
+      link: `/blog/${post.slug}`,
+      content: sanitizeHtml(marked.parse(post.body)),
+      ...post.data,
+    })),
   });
 }
