18528591d3ffa4e223b5878643e2d1b92671b61e
3DRX <3drxkjy@gmail.com>
2024-06-27 13:39:52 +0800

    update rss config

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,22 +1,25 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET(context) {
-  const posts = await getCollection("blog");
+  const blog = await getCollection("blog");
   return rss({
     title: "3DRX’s Blog",
     description: "A random dude on the internet",
     site: context.site,
-    items: posts
+    items: blog
       .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
-      .map((post) => {
-        return {
-          title: post.data.title,
-          description: post.data.description,
-          pubDate: post.data.pubDate,
-          link: `/blog/${post.slug}`,
-          content: post.body,
-        };
-      }),
+      .map((post) => ({
+        title: post.data.title,
+        description: post.data.description,
+        pubDate: post.data.pubDate,
+        link: `/blog/${post.slug}`,
+        content: sanitizeHtml(parser.render(post.body), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+        }),
+      })),
   });
 }
