5b2bd0f8d1eca9aa140399d67e2b286cd8a1ef91
3DRX <kjy20030228@icloud.com>
2025-01-31 22:20:36 +0800

    I think I solved all the styling problems

--- tailwind.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{astro,js,jsx,md,mdx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
